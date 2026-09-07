bd3a7837921f3d759d505bc69a9182f4a52ce437
matsufriends <pikapikan0526@gmail.com>
2026-09-07 21:55:26 +0900

    MornDesktopTube を v0.5.1 に更新

--- Casks/morndesktoptube.rb
+++ Casks/morndesktoptube.rb
@@ -1,6 +1,6 @@
 cask "morndesktoptube" do
-  version "0.5.0"
-  sha256 "a9f15f144771134cd426f81468a9407b5c6297877741b8cffa8983b897841c28"
+  version "0.5.1"
+  sha256 "dda1943c28591756363c9e16affcad3b8c4b8b6e94e03b596265fde3b36328da"
 
   url "https://github.com/TsukumiStudio/MornDesktopTube/releases/download/v#{version}/MornDesktopTube.app.zip"
   name "MornDesktopTube"
